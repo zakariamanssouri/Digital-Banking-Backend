d291a580325a57d899b84dcfe1805bddbc4cadca
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-15 04:15:39 +0100

    add BankAccountRepository

---
+++ src/main/java/ma/enset/digitalbanking/repositories/BankAccountRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.digitalbanking.repositories;
+
+import ma.enset.digitalbanking.entities.BankAccount;
+import ma.enset.digitalbanking.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount,String>{
+}
