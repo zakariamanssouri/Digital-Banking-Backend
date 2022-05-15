1283ae3924299d0ea3c1e388dd02153017beec06
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-15 03:28:14 +0100

    add Customer entity

---
+++ src/main/java/ma/enset/digitalbanking/entities/Customer.java
@@ -0,0 +1,20 @@
+package ma.enset.digitalbanking.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "customer")
+    private List<BankAccount> bankAccounts;
+}
