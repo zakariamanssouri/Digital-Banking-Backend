06625fdc36eaa994a815f76f0d07b95e0d0e10f2
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-15 03:28:55 +0100

    add BanAccount entity

---
+++ src/main/java/ma/enset/digitalbanking/entities/BankAccount.java
@@ -0,0 +1,29 @@
+package ma.enset.digitalbanking.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ma.enset.digitalbanking.enums.AccountStatus;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE",length = 4,discriminatorType = DiscriminatorType.STRING)
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private double balance;
+    private Date createdAt;
+    private AccountStatus status;
+
+    @ManyToOne
+    private Customer customer;
+
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperations;
+
+}
