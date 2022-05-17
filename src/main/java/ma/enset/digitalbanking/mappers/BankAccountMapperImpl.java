c83d8f6a7fd234d64a75f33db47c021ea358aa7a
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-17 23:36:24 +0100

    add mapper to transform customer to customerDTO

---
+++ src/main/java/ma/enset/digitalbanking/mappers/BankAccountMapperImpl.java
@@ -0,0 +1,21 @@
+package ma.enset.digitalbanking.mappers;
+
+import ma.enset.digitalbanking.dtos.CustomerDTO;
+import ma.enset.digitalbanking.entities.Customer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankAccountMapperImpl {
+    public CustomerDTO fromCustomer(Customer customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer,customerDTO);
+        return customerDTO;
+    }
+
+    public Customer fromCustomerDTO(CustomerDTO customerDTO) {
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO, customer);
+        return customer;
+    }
+}
