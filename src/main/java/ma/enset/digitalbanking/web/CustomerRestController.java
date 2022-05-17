b0ff058cfc5fddd3735dff73494da1ba9d8d2cd5
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-18 00:11:24 +0100

    add routes for add , get , update & delete customers

--- src/main/java/ma/enset/digitalbanking/web/CustomerRestController.java
+++ src/main/java/ma/enset/digitalbanking/web/CustomerRestController.java
@@ -5,10 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.enset.digitalbanking.dtos.CustomerDTO;
 import ma.enset.digitalbanking.entities.Customer;
+import ma.enset.digitalbanking.exceptions.CustomerNotFoundException;
 import ma.enset.digitalbanking.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +22,26 @@ public class CustomerRestController {
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     }
+
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long id) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(id);
+    }
+
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomerDTO(@RequestBody CustomerDTO customer) {
+        return bankAccountService.saveCustomer(customer);
+    }
+
+    @PutMapping("/customers/{customersId}")
+    public CustomerDTO updateCustomer(@RequestBody CustomerDTO customerDTO,@PathVariable(name = "customersId") Long id) {
+        customerDTO.setId(id);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    void deleteCustomer(@PathVariable(name = "id") Long id) {
+        bankAccountService.deleteCustomer(id);
+    }
+
 }
