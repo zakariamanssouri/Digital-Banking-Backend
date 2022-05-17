1d96c636eaa42213747ae91dc94578af44899f6d
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-17 23:35:45 +0100

    add CustomerDTO

---
+++ src/main/java/ma/enset/digitalbanking/dtos/CustomerDTO.java
@@ -0,0 +1,12 @@
+package ma.enset.digitalbanking.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class CustomerDTO {
+    private Long id;
+    private String name;
+    private String email;
+}
