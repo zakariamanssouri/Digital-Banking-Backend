71e5e6305239e97441d3f89af033b30beaef5eb7
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-17 23:03:27 +0100

    add BankAccountService interface

---
+++ src/main/java/ma/enset/digitalbanking/services/BankAccountService.java
@@ -0,0 +1,25 @@
+package ma.enset.digitalbanking.services;
+
+import ma.enset.digitalbanking.entities.BankAccount;
+import ma.enset.digitalbanking.entities.CurrentAccount;
+import ma.enset.digitalbanking.entities.Customer;
+import ma.enset.digitalbanking.entities.SavingAccount;
+import ma.enset.digitalbanking.enums.OperationType;
+import ma.enset.digitalbanking.exceptions.BalanceNotSufficientException;
+import ma.enset.digitalbanking.exceptions.BankAccountNotFoundException;
+import ma.enset.digitalbanking.exceptions.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface BankAccountService {
+    Customer saveCustomer(Customer customer);
+    CurrentAccount saveCurrentBankAccount(Double initialBalance, double overDraft, Long customerId) throws CustomerNotFoundException;
+    SavingAccount saveSavingBankAccount(Double initialBalance, double interestRate, Long customerId) throws CustomerNotFoundException;
+    List<Customer> listCustomers();
+    BankAccount getBankAccount(String accountId) throws BankAccountNotFoundException;
+    void debit(String accountId,double amount,String description) throws BankAccountNotFoundException, BalanceNotSufficientException;
+    void credit(String accountId,double amount,String description) throws BankAccountNotFoundException;
+    void transfer(String accountIdSource, String accountIdDestination, double amount) throws BankAccountNotFoundException, BalanceNotSufficientException;
+
+    List<BankAccount> bankAccountsList();
+}
